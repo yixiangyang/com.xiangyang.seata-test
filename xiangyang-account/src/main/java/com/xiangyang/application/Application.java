0792a29126a6edbdd92b22ba5647672641bb399e
yixiangyang <15138778469@163.com>
2019-12-06 15:56:42 +0800

    aa

--- xiangyang-account/src/main/java/com/xiangyang/application/Application.java
+++ xiangyang-account/src/main/java/com/xiangyang/application/Application.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import com.yixiangyang.controller.MyBatisConfig;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = { "com.yixiangyang.controller", "com.yixiangyang.service" })
 @org.springframework.cloud.netflix.feign.EnableFeignClients(basePackages = {"com.csp.tp.client","com.csp.tp.feign" })
 @MapperScan(basePackages= {"com.yixiangyang.mapper"})
 @EnableEurekaClient
+//@Import(MyBatisConfig.class)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- xiangyang-account/src/main/java/com/yixiangyang/controller/MyBatisConfig.java
+++ xiangyang-account/src/main/java/com/yixiangyang/controller/MyBatisConfig.java
@@ -1,6 +1,7 @@
 package com.yixiangyang.controller;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,32 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 @Configuration
 public class MyBatisConfig {
+	 private DataSourceProperties dataSourceProperties;
+	 
+	 public MyBatisConfig(DataSourceProperties dataSourceProperties) {
+		  this.dataSourceProperties = dataSourceProperties;
+	 }
+
 	 @Bean
-	    @ConfigurationProperties(prefix = "spring.datasource")
+//	    @ConfigurationProperties(prefix = "spring.datasource")
 	    public DataSource druidDataSource(){
-	        DruidDataSource druidDataSource = new DruidDataSource();
-	        return druidDataSource;
+//	        DruidDataSource druidDataSource = new DruidDataSource();
+		 	HikariDataSource dataSource = new HikariDataSource();
+	        System.out.println(dataSourceProperties.getUrl());
+	        System.out.println(dataSourceProperties.getUsername());
+	        System.out.println(dataSourceProperties.getPassword());
+	        System.out.println(dataSourceProperties.getDriverClassName());
+	        dataSource.setJdbcUrl(dataSourceProperties.getUrl());
+	        dataSource.setUsername(dataSourceProperties.getUsername());
+	        dataSource.setPassword(dataSourceProperties.getPassword());
+	        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+	        return dataSource;
 	    }
 
 	    @Primary
@@ -39,4 +56,5 @@ public class MyBatisConfig {
 	        sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
 	        return sqlSessionFactoryBean.getObject();
 	    }
+
 }
